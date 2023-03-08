ad9af2dbf98e0e4dced26990c4abe810442e6d91
matercomus <matek.kedzia@gmail.com>
2023-03-09 00:00:45 +0100

    refactoring smartpynector.py and using to create ThermostatSC.ipynb

--- smartpynector.py
+++ smartpynector.py
@@ -1,7 +1,7 @@
-'''
+"""
 This is a python wrapper for the Knowledge Engine Smart Connector REST API.
 See https://gitlab.inesctec.pt/interconnect-public/knowledge-engine/-/blob/main/openapi-sc.yaml
-'''
+"""
 
 import logging
 
@@ -26,74 +26,33 @@ class SmartConnector(BaseModel):
 def check_request_status(r):
     if r.status_code == 200:
         logging.info('Request successful')
+        logging.debug(r.text)
     else:
         logging.error('Error in request')
         logging.error(r.text)
 
 
-# TODO: FIX THIS
-def register_ask_ki(knowledge_engine_url: AnyUrl, prefixes: dict, graph_pattern: str, headers: dict):
-    # data for the AskKnowledgeInteraction
-    ask_ki_data = {
-        "knowledgeInteractionType": "AskKnowledgeInteraction",
+# TODO: add support for other types of Knowledge Interactions
+def register_knowledge_interaction(knowledge_interaction_type: str, knowledge_engine_url: AnyUrl, prefixes: dict,
+                                   graph_pattern: str, headers: dict):
+    # check if the knowledge_interaction_type is valid
+    ki_types = ['AskKnowledgeInteraction', 'AnswerKnowledgeInteraction']
+    if knowledge_interaction_type not in ki_types:
+        raise ValueError('knowledge_interaction_type must be one of the following: ' + str(ki_types))
+
+    # data of the Knowledge Interaction
+    ki_data = {
+        "knowledgeInteractionType": knowledge_interaction_type,
         "prefixes": prefixes,
         "graphPattern": graph_pattern,
     }
 
-    r = httpx.post(knowledge_engine_url + '/sc/ki', headers=headers, json=ask_ki_data)
+    r = httpx.post(knowledge_engine_url + '/sc/ki', headers=headers, json=ki_data)
 
     # check if the request was successful
     check_request_status(r)
 
     return r
-
-
-def register_answer_ki(knowledge_engine_url: AnyUrl, prefixes: dict, graph_pattern: str, headers: dict):
-    # data for the AnswerKnowledgeInteraction
-    answer_ki_data = {
-        "knowledgeInteractionType": "AnswerKnowledgeInteraction",
-        "prefixes": prefixes,
-        "graphPattern": graph_pattern,
-    }
-
-    r = httpx.post(knowledge_engine_url + '/sc/ki', headers=headers, json=answer_ki_data)
-
-    # check if the request was successful
-    check_request_status(r)
-    return r
-
-
-def register_knowledge_interaction(
-        knowledge_engine_url: AnyUrl,
-        knowledge_base_id: AnyUrl,
-        knowledge_interaction_type: str,
-        prefixes: dict,
-        graph_pattern: str,
-):
-    """
-    Register a Knowledge Interaction for a Smart Connector
-    :param knowledge_engine_url: url of the Knowledge Engine
-    :param knowledge_base_id: id of the Knowledge Base of the Smart Connector
-    :param knowledge_interaction_type: type of Knowledge Interaction [AskKnowledgeInteraction,
-                                                                        AnswerKnowledgeInteraction, more to come]
-    :param prefixes: prefixes used in the graph pattern
-    :param graph_pattern: the graph pattern of the Knowledge Interaction in SPARQL as a string
-    :return: response of the request
-    """
-    headers = {
-        'Content-Type': 'application/json',
-        'Knowledge-Base-Id': knowledge_base_id,
-    }
-
-    if knowledge_interaction_type == 'AskKnowledgeInteraction':
-        register_ask_ki(knowledge_engine_url, prefixes, graph_pattern, headers)
-
-    elif knowledge_interaction_type == 'AnswerKnowledgeInteraction':
-        register_answer_ki(knowledge_engine_url, prefixes, graph_pattern, headers)
-
-    else:
-        logging.error(f'Knowledge Interaction type {knowledge_interaction_type} not supported')
-        return None
 
 
 def create_smart_connector(smart_connector_obj: SmartConnector, knowledge_engine_url: AnyUrl):
@@ -110,10 +69,6 @@ def create_smart_connector(smart_connector_obj: SmartConnector, knowledge_engine
     r = httpx.post(knowledge_engine_url + '/sc', headers=headers, json=smart_connector_obj.dict())
 
     # check if the request was successful
-    if r.status_code == 200:
-        logging.info('Smart Connector created successfully')
-    else:
-        logging.error('Error creating Smart Connector')
-        logging.error(r.text)
+    check_request_status(r)
 
     return r
