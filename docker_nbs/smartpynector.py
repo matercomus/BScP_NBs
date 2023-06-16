cd204baf82afcca83a8e959adec9ddb3039df5fb
matek.kedzia@gmail.com <matek.kedzia@gmail.com>
2023-06-16 20:03:46 +0000

    added TUI and minor changes to pyconector

--- docker_nbs/smartpynector.py
+++ docker_nbs/smartpynector.py
@@ -17,7 +17,7 @@ from rdflib.plugins.stores.sparqlstore import SPARQLUpdateStore, Store
 
 # set up logging
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
     format='%(asctime)s %(levelname)s %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
 )
@@ -34,15 +34,6 @@ def get_timestamp_now():
     return datetime.datetime.now().astimezone().replace(microsecond=0).isoformat()  # ISO 8601
 
 
-def check_request_status(r):
-    if r.status_code == 200:
-        logging.info('Request successful')
-        logging.debug(r.text)
-    else:
-        logging.error('Error in request')
-        logging.error(r.text)
-
-
 def create_smart_connector(smart_connector_obj: SmartConnector, knowledge_engine_url: str):
     """
     Create a new Smart Connector for the given Knowledge Base.
@@ -56,42 +47,49 @@ def create_smart_connector(smart_connector_obj: SmartConnector, knowledge_engine
     }
     r = requests.post(knowledge_engine_url + '/sc', headers=headers, json=smart_connector_obj.dict())
 
-    # check if the request was successful
-    check_request_status(r)
-
     return r
 
 
-# TODO: add support for other types of Knowledge Interactions
 def register_knowledge_interaction(knowledge_interaction_type: str, knowledge_engine_url: AnyUrl, prefixes: dict = None,
                                    graph_pattern: str = None, headers: dict = None, argument_graph_pattern: str = None,
-                                   result_graph_pattern: str = None):
+                                   result_graph_pattern: str = None, ki_name: str = None, kb_id: str = None):
     # check if the knowledge_interaction_type is valid
-    ki_types = ['AskKnowledgeInteraction', 'AnswerKnowledgeInteraction', "ReactKnowledgeInteraction"]
+    ki_types = ['AskKnowledgeInteraction', 'AnswerKnowledgeInteraction', "ReactKnowledgeInteraction", "PostKnowledgeInteraction"]
     if knowledge_interaction_type not in ki_types:
         raise ValueError('knowledge_interaction_type must be one of the following: ' + str(ki_types))
 
     # data of the Knowledge Interaction
     if knowledge_interaction_type in ['AskKnowledgeInteraction', 'AnswerKnowledgeInteraction']:
         ki_data = {
+            "knowledgeInteractionName": ki_name,
             "knowledgeInteractionType": knowledge_interaction_type,
             "prefixes": prefixes,
             "graphPattern": graph_pattern,
         }
     elif knowledge_interaction_type == "ReactKnowledgeInteraction":
         ki_data = {
+            "knowledgeInteractionName": ki_name,
             "knowledgeInteractionType": knowledge_interaction_type,
             "argumentGraphPattern": argument_graph_pattern,
             "resultGraphPattern": result_graph_pattern,
             "prefixes": prefixes,
         }
+    elif knowledge_interaction_type == "PostKnowledgeInteraction":
+        ki_data = {
+            "knowledgeInteractionName": ki_name,
+            "knowledgeInteractionType": knowledge_interaction_type,
+            "argumentGraphPattern": argument_graph_pattern,
+            "prefixes": prefixes,
+        }
+        if result_graph_pattern is not None:
+            ki_data["argumentGraphPattern"] = result_graph_pattern
     else:
         raise ValueError('knowledge_interaction_type must be one of the following: ' + str(ki_types))
 
     r = requests.post(knowledge_engine_url + '/sc/ki', headers=headers, json=ki_data)
 
     # check if the request was successful
-    check_request_status(r)
+    assert r.ok
 
     return r
 
@@ -106,46 +104,62 @@ def perform_ask_knowledge_interaction(knowledge_engine_url: AnyUrl, knowledge_ba
     r = requests.post(knowledge_engine_url + '/sc/ask', headers=headers, json=ask_data)
 
     # check if the request was successful
-    check_request_status(r)
+    assert r.ok
 
     return r
 
 
 def perform_answer_knowledge_interaction(knowledge_engine_url: AnyUrl, knowledge_base_id: AnyUrl,
                                          knowledge_interaction_id: AnyUrl, answer_binding_set: list[dict[str, str]]):
-    # get a handle request id
-    handle_headers = {
-        "Content-Type": "application/json",
-        "Knowledge-Base-Id": knowledge_base_id,
-    }
-    # create a session with retries to avoid 202 errors
-    # s = requests.Session()
-    # retries = Retry(backoff_factor=1, status_forcelist=[i for i in range(202, 500)])
-    # s.mount('https://', HTTPAdapter(max_retries=retries))
+    while True:
+        # get a handle request id
+        handle_headers = {
+            "Content-Type": "application/json",
+            "Knowledge-Base-Id": knowledge_base_id,
+        }
 
-    # make the handle request
-    handle_request = requests.get(knowledge_engine_url + '/sc/handle', headers=handle_headers, timeout=None)
+        # make the handle request
+        handle_request = requests.get(knowledge_engine_url + '/sc/handle', headers=handle_headers, timeout=None)
 
-    # check if the request was successful
-    # check_request_status(handle_request)
+        if handle_request.status_code == 200:
+            handle_request_id = handle_request.json()['handleRequestId']
 
-    handle_request_id = handle_request.json()['handleRequestId']
+            # answer the Knowledge Interaction
+            answer_headers = {
+                "Content-Type": "application/json",
+                "Knowledge-Base-Id": knowledge_base_id,
+                "Knowledge-Interaction-Id": knowledge_interaction_id,
+            }
+            answer_data = {
+                "handleRequestId": handle_request_id,
+                "bindingSet": answer_binding_set,
+            }
+            r = requests.post(knowledge_engine_url + '/sc/handle', headers=answer_headers, json=answer_data)
 
-    # answer the Knowledge Interaction
-    answer_headers = {
-        "Content-Type": "application/json",
-        "Knowledge-Base-Id": knowledge_base_id,
-        "Knowledge-Interaction-Id": knowledge_interaction_id,
-    }
-    answer_data = {
-        "handleRequestId": handle_request_id,
-        "bindingSet": answer_binding_set,
-    }
-    r = requests.post(knowledge_engine_url + '/sc/handle', headers=answer_headers, json=answer_data)
-    # check if the request was successful
-    # check_request_status(r)
+            if r.ok:
+                return r
+            else:
+                logger.warn(f"received unexpected status {r.status_code}")
+                logger.warn(r.text)
+                logger.info("retrying after a short timeout")
+                time.sleep(2)
+                continue
+        elif handle_request.status_code == 202:
+            # 202 means: repoll (heartbeat)
+            continue
+        elif handle_request.status_code == 410:
+            # 410 means: KE has stopped, so terminate
+            break
+        else:
+            logger.warn(f"received unexpected status {handle_request.status_code}")
+            logger.warn(handle_request.text)
+            logger.info("retrying after a short timeout")
+            time.sleep(2)
+            continue
 
-    return r
+    logger.info(f"exiting perform_answer_knowledge_interaction")
+
+    
 
 
 def inject_binding_set_into_graph_pattern(graph_pattern: str, binding_set: list[dict[str, str]]) -> list[str]:
