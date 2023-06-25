d7051f3de36edf3266b2342c9f42e06796de091e
matercomus <matek.kedzia@gmail.com>
2023-06-25 23:00:56 +0000

    moved dockernbs to root of repo and deleted unused nbs

--- smartpynector.py
+++ smartpynector.py
@@ -1,15 +1,11 @@
-"""
-This is a python wrapper for the Knowledge Engine Smart Connector REST API.
-See https://gitlab.inesctec.pt/interconnect-public/knowledge-engine/-/blob/main/openapi-sc.yaml
-"""
-
 import datetime
+import time
 import logging
-from typing import Any, Dict, Optional
-
 import pandas as pd
 import rdflib as rdflib
 import requests
+
+from typing import Any, Dict, Optional
 from SPARQLWrapper import SPARQLWrapper
 from pydantic import BaseModel, AnyUrl
 from rdflib import Graph, URIRef
@@ -17,135 +13,14 @@ from rdflib.plugins.stores.sparqlstore import SPARQLUpdateStore, Store
 
 # set up logging
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
     format='%(asctime)s %(levelname)s %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
 )
 
 
-class SmartConnector(BaseModel):
-    knowledgeBaseId: AnyUrl
-    knowledgeBaseName: str
-    knowledgeBaseDescription: str
-    reasonerEnabled: bool
-
-
 def get_timestamp_now():
     return datetime.datetime.now().astimezone().replace(microsecond=0).isoformat()  # ISO 8601
-
-
-def check_request_status(r):
-    if r.status_code == 200:
-        logging.info('Request successful')
-        logging.debug(r.text)
-    else:
-        logging.error('Error in request')
-        logging.error(r.text)
-
-
-def create_smart_connector(smart_connector_obj: SmartConnector, knowledge_engine_url: AnyUrl):
-    """
-    Create a new Smart Connector for the given Knowledge Base.
-    :param smart_connector_obj: smart connector object
-    :param knowledge_engine_url: url of the Knowledge Engine
-    :return: response of the request
-    """
-    headers = {
-        'Content-Type': 'application/json',
-        'Knowledge-Base-Id': smart_connector_obj.knowledgeBaseId,
-    }
-    r = requests.post(knowledge_engine_url + '/sc', headers=headers, json=smart_connector_obj.dict())
-
-    # check if the request was successful
-    check_request_status(r)
-
-    return r
-
-
-# TODO: add support for other types of Knowledge Interactions
-def register_knowledge_interaction(knowledge_interaction_type: str, knowledge_engine_url: AnyUrl, prefixes: dict = None,
-                                   graph_pattern: str = None, headers: dict = None, argument_graph_pattern: str = None,
-                                   result_graph_pattern: str = None):
-    # check if the knowledge_interaction_type is valid
-    ki_types = ['AskKnowledgeInteraction', 'AnswerKnowledgeInteraction', "ReactKnowledgeInteraction"]
-    if knowledge_interaction_type not in ki_types:
-        raise ValueError('knowledge_interaction_type must be one of the following: ' + str(ki_types))
-
-    # data of the Knowledge Interaction
-    if knowledge_interaction_type in ['AskKnowledgeInteraction', 'AnswerKnowledgeInteraction']:
-        ki_data = {
-            "knowledgeInteractionType": knowledge_interaction_type,
-            "prefixes": prefixes,
-            "graphPattern": graph_pattern,
-        }
-    elif knowledge_interaction_type == "ReactKnowledgeInteraction":
-        ki_data = {
-            "knowledgeInteractionType": knowledge_interaction_type,
-            "argumentGraphPattern": argument_graph_pattern,
-            "resultGraphPattern": result_graph_pattern,
-            "prefixes": prefixes,
-        }
-    else:
-        raise ValueError('knowledge_interaction_type must be one of the following: ' + str(ki_types))
-
-    r = requests.post(knowledge_engine_url + '/sc/ki', headers=headers, json=ki_data)
-
-    # check if the request was successful
-    check_request_status(r)
-
-    return r
-
-
-def perform_ask_knowledge_interaction(knowledge_engine_url: AnyUrl, knowledge_base_id: AnyUrl,
-                                      knowledge_interaction_id: AnyUrl, ask_data: list[dict[str, str]]):
-    headers = {
-        "Knowledge-Base-Id": knowledge_base_id,
-        "Knowledge-Interaction-Id": knowledge_interaction_id,
-    }
-
-    r = requests.post(knowledge_engine_url + '/sc/ask', headers=headers, json=ask_data)
-
-    # check if the request was successful
-    check_request_status(r)
-
-    return r
-
-
-def perform_answer_knowledge_interaction(knowledge_engine_url: AnyUrl, knowledge_base_id: AnyUrl,
-                                         knowledge_interaction_id: AnyUrl, answer_binding_set: list[dict[str, str]]):
-    # get a handle request id
-    handle_headers = {
-        "Content-Type": "application/json",
-        "Knowledge-Base-Id": knowledge_base_id,
-    }
-    # create a session with retries to avoid 202 errors
-    # s = requests.Session()
-    # retries = Retry(backoff_factor=1, status_forcelist=[i for i in range(202, 500)])
-    # s.mount('https://', HTTPAdapter(max_retries=retries))
-
-    # make the handle request
-    handle_request = requests.get(knowledge_engine_url + '/sc/handle', headers=handle_headers, timeout=None)
-
-    # check if the request was successful
-    # check_request_status(handle_request)
-
-    handle_request_id = handle_request.json()['handleRequestId']
-
-    # answer the Knowledge Interaction
-    answer_headers = {
-        "Content-Type": "application/json",
-        "Knowledge-Base-Id": knowledge_base_id,
-        "Knowledge-Interaction-Id": knowledge_interaction_id,
-    }
-    answer_data = {
-        "handleRequestId": handle_request_id,
-        "bindingSet": answer_binding_set,
-    }
-    r = requests.post(knowledge_engine_url + '/sc/handle', headers=answer_headers, json=answer_data)
-    # check if the request was successful
-    # check_request_status(r)
-
-    return r
 
 
 def inject_binding_set_into_graph_pattern(graph_pattern: str, binding_set: list[dict[str, str]]) -> list[str]:
@@ -288,3 +163,10 @@ def convert_sparql_results_to_graph(
                 o = rdflib.Literal(result[object_var]["value"])
             g.add((s, p, o))
     return g
+
+def convert_graph_pattern_to_sparql(graph_pattern, prefixes):
+    prefix_str = ""
+    for prefix, uri in prefixes.items():
+        prefix_str += f"PREFIX {prefix}: <{uri}>\n"
+    query = f"{prefix_str}\nSELECT *\nWHERE {{\n{graph_pattern}\n}}"
+    return query
