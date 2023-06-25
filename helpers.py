d704b91591aba49d16da387be6ab99287fe2cc29
matercomus <matek.kedzia@gmail.com>
2023-06-25 23:29:31 +0000

    renamed smartpynector to helpers and updated the import in all nbs

--- helpers.py
+++ helpers.py
@@ -1,3 +1,5 @@
+# SPARQL and GraphDB helper functions
+
 import datetime
 import time
 import logging
@@ -20,10 +22,20 @@ logging.basicConfig(
 
 
 def get_timestamp_now():
+    """
+    Returns the current timestamp in ISO 8601 format.
+    """
     return datetime.datetime.now().astimezone().replace(microsecond=0).isoformat()  # ISO 8601
 
 
 def inject_binding_set_into_graph_pattern(graph_pattern: str, binding_set: list[dict[str, str]]) -> list[str]:
+    """
+    Injects a binding set into a graph pattern.
+
+    :param graph_pattern: A string representing the graph pattern.
+    :param binding_set: A list of dictionaries representing the binding set.
+    :return: A list of strings representing the graph pattern with the binding set injected.
+    """
     # check if the binding set is empty
     if binding_set in [None, []]:
         raise ValueError('binding_set cannot be empty')
@@ -39,6 +51,13 @@ def inject_binding_set_into_graph_pattern(graph_pattern: str, binding_set: list[
 
 
 def replace_prefixes_with_uris(graph_pattern: list[str], prefixes: dict) -> list[str | Any]:
+    """
+    Replaces prefixes with URIs in a given graph pattern.
+
+    :param graph_pattern: A list of strings representing the graph pattern.
+    :param prefixes: A dictionary mapping prefixes to URIs.
+    :return: A list of strings representing the graph pattern with prefixes replaced by URIs.
+    """
     # check if the prefixes dictionary is empty
     if prefixes in [None, {}]:
         raise ValueError('prefixes cannot be empty')
@@ -52,6 +71,14 @@ def replace_prefixes_with_uris(graph_pattern: list[str], prefixes: dict) -> list
 
 
 def convert_to_turtle_rdf(graph_pattern: str, binding_set: list[dict[str, str]], prefixes: dict) -> str:
+    """
+    Converts a given graph pattern to Turtle RDF format.
+
+    :param graph_pattern: A string representing the graph pattern.
+    :param binding_set: A list of dictionaries representing the binding set.
+    :param prefixes: A dictionary mapping prefixes to URIs.
+    :return: A string representing the Turtle RDF format of the given graph pattern.
+    """
     rdf = inject_binding_set_into_graph_pattern(graph_pattern, binding_set)
     rdf = replace_prefixes_with_uris(rdf, prefixes)
     result = []
@@ -66,21 +93,41 @@ def convert_to_turtle_rdf(graph_pattern: str, binding_set: list[dict[str, str]],
 
 
 def set_store_header_update(store: Store):
+    """
+    Sets the appropriate request headers for update operations on a given store.
+
+    :param store: The store object to set headers on.
+    """
+    
     """Call this function before any `Graph.add()` calls to set the appropriate request headers."""
     if 'headers' not in store.kwargs:
         store.kwargs.update({'headers': {}})
-    store.kwargs['headers'].update({'content-type': 'application/sparql-update'})
+        store.kwargs['headers'].update({'content-type': 'application/sparql-update'})
 
 
 def set_store_header_read(store: Store):
+    """
+    Sets the appropriate request headers for read operations on a given store.
+
+    :param store: The store object to set headers on.
+    """
     """Call this function before any `Graph.triples()` calls to set the appropriate request headers."""
     if 'headers' not in store.kwargs:
         store.kwargs.update({'headers': {}})
-    store.kwargs['headers'].pop('content-type', None)
+        store.kwargs['headers'].pop('content-type', None)
 
 
 def store_data_in_graphdb(graph_pattern: str, binding_set: list[dict[str, str]],
                           prefixes: dict, read_url: str, write_url: str):
+    """
+    Store data in a graph database using the given graph pattern and binding set.
+
+    :param graph_pattern: A string representing the graph pattern to use for storing the data.
+    :param binding_set: A list of dictionaries representing the binding set to use for storing the data.
+    :param prefixes: A dictionary of prefixes to use when converting the graph pattern to turtle RDF format.
+    :param read_url: The URL of the SPARQL endpoint to use for reading data from the graph database.
+    :param write_url: The URL of the SPARQL endpoint to use for writing data to the graph database.
+    """
     turtle_rdf = convert_to_turtle_rdf(graph_pattern, binding_set, prefixes)
     store = SPARQLUpdateStore(query_endpoint=read_url, update_endpoint=write_url,
                               context_aware=True, postAsEncoded=False)
@@ -100,6 +147,13 @@ def store_data_in_graphdb(graph_pattern: str, binding_set: list[dict[str, str]],
 
 
 def read_triples_from_graphdb(read_url: str, write_url: str):
+    """
+    Read triples from a graph database.
+
+    :param read_url: The URL of the SPARQL endpoint to use for reading data from the graph database.
+    :param write_url: The URL of the SPARQL endpoint to use for writing data to the graph database.
+    :return: An iterator over the triples in the graph database.
+    """
     store = SPARQLUpdateStore(query_endpoint=read_url, update_endpoint=write_url,
                               context_aware=True, postAsEncoded=False)
     store.debug = True
@@ -111,6 +165,14 @@ def read_triples_from_graphdb(read_url: str, write_url: str):
 
 
 def run_sparql_query(endpoint, query, return_format: str = "json"):
+    """
+    Run a SPARQL query against a given endpoint and return the results.
+
+    :param endpoint: The URL of the SPARQL endpoint to query.
+    :param query: The SPARQL query to run.
+    :param return_format: The format in which to return the results (default is "json").
+    :return: The results of the SPARQL query in the specified format.
+    """
     sparql = SPARQLWrapper(endpoint)
     sparql.setQuery(query)
     sparql.setReturnFormat(return_format.lower())
@@ -119,6 +181,14 @@ def run_sparql_query(endpoint, query, return_format: str = "json"):
 
 
 def sparql_results_to_df(results):
+    """
+    Convert SPARQL query results in JSON format into a pandas DataFrame.
+
+    :param results: The result of a SPARQL query in JSON format.
+    :return: A pandas DataFrame containing the data from the SPARQL query results.
+    """
+    
+
     cols = results['head']['vars']
     out = []
     for row in results['results']['bindings']:
